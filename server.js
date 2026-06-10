757f7315e7cd150916b04587c75d17257086f2bc
MaygarPosta <maygarposta@proton.me>
2026-06-10 10:49:29 -0400

    fix: cors abierto todos los origenes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:4500', 'https://marealestate-frontend.vercel.app'],
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type']
 }));
